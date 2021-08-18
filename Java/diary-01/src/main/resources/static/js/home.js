8f5bbb621df814e2ab4686b893187e8bfd0c5a5d
Akane--33 <akane.komagata@seattleconsulting.co.jp>
2021-08-18 15:28:21 +0900

    一覧画面の実装が完了しました

---
+++ Java/diary-01/src/main/resources/static/js/home.js
@@ -0,0 +1,33 @@
+/**
+ * 日記の日付と日記のタイトルデータの取得
+ */
+window.onload = OnLinkClick();
+
+function OnLinkClick(){
+	var xmlHttpRequest = new XMLHttpRequest();
+	
+	xmlHttpRequest.responseType = 'json';
+
+	xmlHttpRequest.open('GET', '/api/inquiry', true);
+	
+	xmlHttpRequest.send();
+
+	xmlHttpRequest.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			diaryDate = this.response;
+    		console.log(diaryDate.date);
+			let table = document.getElementById('diaryTable');
+		
+	
+			diaryDate.forEach(function(data){
+			let newRow = table.insertRow();
+			let newCell = newRow.insertCell();
+			let newText = document.createTextNode(data.date);
+			newCell.appendChild(newText);
+			let newTitle = document.createTextNode(data.title);
+			let Title = newRow.insertCell();
+			Title.appendChild(newTitle);
+			});
+		}
+	}
+}
